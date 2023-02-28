176dedb85b954e3c844f925d621ce4b5330de0df
KelvinWong0421 <kelvinwong159357@gmail.com>
2023-02-28 11:56:02 +0800

    Change to tailwind.css

---
+++ pages/component/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Link from 'next/link'
+
+type Props = {}
+
+
+function Header({}: Props) {
+  return (
+    <>
+    
+    </>
+  )
+}
